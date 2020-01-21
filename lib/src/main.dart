905fe4ff563fd7cd38703c8215edac17f9c3dffe
Fernando Acosta <fernandotovar9902@gmail.com>
2020-01-21 14:51:17 -0600

    detalles menores

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,6 +1,8 @@
 import 'package:desings/src/pages/basic_page.dart';
 import 'package:desings/src/pages/buttons_page.dart';
 import 'package:desings/src/pages/main_page.dart';
+import 'package:desings/src/pages/mytries/login_page.dart';
+import 'package:desings/src/pages/mytries/recipies_page.dart';
 import 'package:desings/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,10 +23,12 @@ class MyApp extends StatelessWidget {
       
       initialRoute: 'main',
       routes: {
+        'main' : (context) => MainPage(),
         'basic' : (context) => BasicPage(),
         'scroll' : (context) => ScrollPage(),
         'buttons' : (context) => ButtonsPage(),
-        'main' : (context) => MainPage(),
+        'recipies' : (context) => RecipiesPage(),
+        'login' : (context) => LoginPage(),
       },
       
     );
--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:desings/src/pages/basic_page.dart';
 import 'package:desings/src/pages/buttons_page.dart';
+import 'package:desings/src/pages/mytries/login_page.dart';
+import 'package:desings/src/pages/mytries/recipies_page.dart';
 import 'package:desings/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,8 @@ class MainPage extends StatelessWidget {
            BasicPage(),
            ScrollPage(),
            ButtonsPage(),
+           RecipiesPage(),
+           LoginPage(),
          ],
       );
   }
